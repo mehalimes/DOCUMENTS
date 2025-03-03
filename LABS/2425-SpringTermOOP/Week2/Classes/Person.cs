8148e82adb9d7a639571c88ce383ce0707fa2563
lemniscade <memin1598@gmail.com>
2025-03-03 17:57:12 +0300

    labs

---
+++ LABS/2425-SpringTermOOP/Week2/Classes/Person.cs
@@ -0,0 +1,22 @@
+﻿namespace Week2.Classes
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        protected byte Age { get; set; }
+        public Person(string Name, byte Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+            Console.WriteLine("Person constructor has been called.");
+        }
+        public Person() 
+        {
+            Console.WriteLine("Person parameterless constructor has been called.");
+        }
+        public void ShowInformation()
+        {
+            Console.WriteLine($"INFO {this.Name} {this.Age}");
+        }
+    }
+}
---
+++ LABS/2425-SpringTermOOP/Week2/Program.cs
@@ -0,0 +1,19 @@
+﻿using Week2.Classes;
+
+namespace Week2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("Ali", 15, "23390008003");
+            Student s2 = new Student();
+
+            s1.ShowInformation();
+            s1.ShowStudentInformation();
+
+            s2.ShowInformation();
+            s2.ShowStudentInformation();
+        }
+    }
+}
